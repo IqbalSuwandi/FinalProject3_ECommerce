66cb32777d2252f0bb1f715ad0f797d55cf2b7bb
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2022-07-11 01:18:51 +0700

    Done

---
+++ app/src/main/java/com/example/final_project_3_ecommerce/Admin.java
@@ -0,0 +1,24 @@
+package com.example.final_project_3_ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+
+    public void datastaff(View view) {
+        startActivity(new Intent(Admin.this,DataStaff.class));
+    }
+
+    public void datastock(View view) {
+        startActivity(new Intent(Admin.this,DataStock.class));
+    }
+}
